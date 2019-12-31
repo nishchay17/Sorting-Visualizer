2535346740b998d0d4adf9130f49295c0daae285
nishchay17 <nishchay13971@gmail.com>
2019-12-31 20:01:03 +0530

    added selectionSort

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const container = document.getElementById('container');
 const sort = document.getElementById('sort');
 const shuffle = document.getElementById('shuffle');
 let isSorted = false;
+const green = "#4cd137";
+const blue = "#00a8ff";
+const red = "#e84118";
 
 function createBlocks(number){
-    for (let i = 0; i < number; ++i) {
+    for (let i = 0; i < number; ++i){
         const value = Math.floor(Math.random() * 500);
         const block = document.createElement("div");
         block.classList.add("block");
@@ -22,24 +25,21 @@ function createBlocks(number){
     isSorted = false;
 }
 
-function swap(el1, el2) {
+function swap(el1, el2, from, to, delay = 250){
     return new Promise(resolve => {
-      const style1 = window.getComputedStyle(el1);
-      const style2 = window.getComputedStyle(el2);
-  
-      const transform1 = style1.getPropertyValue("transform");
-      const transform2 = style2.getPropertyValue("transform");
-  
-      el1.style.transform = transform2;
-      el2.style.transform = transform1;
-  
-      // Wait for the transition to end!
-      window.requestAnimationFrame(function() {
-        setTimeout(() => {
-          container.insertBefore(el2, el1);
-          resolve();
-        }, 250);
-      });
+        let val1 =  parseInt(el1.childNodes[0].innerHTML, 10);
+        let val2 =  parseInt(el2.childNodes[0].innerHTML, 10);
+
+        el1.childNodes[0].innerHTML = val2;
+        el2.childNodes[0].innerHTML = val1;
+
+        window.requestAnimationFrame(function() {
+            setTimeout(() => {
+            el1.style.height = val2 + 'px';
+            el2.style.height = val1 + 'px';
+            resolve();
+            }, delay);
+        });
     });
 }
 
@@ -47,25 +47,23 @@ function shuffleIt(){
     let blocks = document.querySelectorAll(".block");
     for (let i = 0; i < 25; ++i) {
         const value = Math.floor(Math.random() * 500);
-        blocks[i].style.backgroundColor = '#00a8ff';
+        blocks[i].style.backgroundColor = blue;
         blocks[i].childNodes[0].innerHTML = value;
         blocks[i].style.height = `${value}px`;
     }
     isSorted = false;
 }
 
-async function bubbleSort(delay = 100){
-    
+async function bubbleSort(delay = 100){  
     if(isSorted)
         return;
-
     let blocks = document.querySelectorAll(".block");
 	flag = false;
 	n = blocks.length;
 	for (let i = 0; i < n; ++i){
 		for (let j = 0 ; j < n - i - 1; ++j){
-            blocks[j].style.backgroundColor = "#e84118";
-            blocks[j + 1].style.backgroundColor = "#e84118";
+            blocks[j].style.backgroundColor = red;
+            blocks[j + 1].style.backgroundColor = red;
 
             await new Promise(resolve =>
                 setTimeout(() => {
@@ -76,14 +74,14 @@ async function bubbleSort(delay = 100){
             let value1 = parseInt(blocks[j].childNodes[0].innerHTML, 10);
             let value2 = parseInt(blocks[j + 1].childNodes[0].innerHTML, 10);
 			if( value2 < value1 ){
-                await swap(blocks[j], blocks[j + 1]);
+                await swap(blocks[j], blocks[j + 1], j, j + 1);
                 blocks = document.querySelectorAll(".block");
                 flag = true;
             }
-            blocks[j].style.backgroundColor = "#00a8ff";
-            blocks[j + 1].style.backgroundColor = "#00a8ff";
+            blocks[j].style.backgroundColor = blue;
+            blocks[j + 1].style.backgroundColor = blue;
         }
-        blocks[blocks.length - i - 1].style.backgroundColor = "#4cd137";
+        blocks[n - i - 1].style.backgroundColor = green;
 
 		if(!flag)
 			break;
@@ -91,7 +89,37 @@ async function bubbleSort(delay = 100){
     isSorted = true;
 }
 
-sort.addEventListener('click', bubbleSort);
-shuffle.addEventListener('click', shuffleIt);
+async function selectionSort(delay = 100){
 
+    if(isSorted)    return;
+
+    let array = document.querySelectorAll(".block");
+    n = array.length;
+    let minIndex;
+
+	for(let i = 0; i < n ; ++i){
+        minIndex = i;
+        array[minIndex].style.backgroundColor = red;
+		for(let j = i + 1; j < n; ++j){
+            array[j].style.backgroundColor = red;
+            let value1 = parseInt(array[j].childNodes[0].innerHTML, 10);
+            let value2 = parseInt(array[minIndex].childNodes[0].innerHTML, 10);
+            if(value1 < value2)  
+                minIndex = j;
+
+            array[j].style.backgroundColor = blue;
+        }
+        array[minIndex].style.backgroundColor = red;
+        await swap(array[minIndex], array[i], minIndex, i);
+        array[minIndex].style.backgroundColor = blue;
+        array[i].style.backgroundColor = green;
+        array = document.querySelectorAll(".block");
+    }
+    isSorted = true;
+}
+
+
+sort.addEventListener('click', bubbleSort);
+// sort.addEventListener('click', selectionSort);
+shuffle.addEventListener('click', shuffleIt);
 createBlocks(25);
